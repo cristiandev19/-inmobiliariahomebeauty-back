4b1a9a923b4915f5b08627163b932883a1659817
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-17 23:36:09 -0500

    quitando redundancia

--- src/utilities/aws.js
+++ src/utilities/aws.js
@@ -2,47 +2,42 @@ const AWS = require('aws-sdk');
 const { config } = require('../config/index');
 
 exports.s3UploadPromise = async ({
-  fileName, extension, bufferBody, contentType
-}) => {
-  return new Promise((resolve) => {
+  fileName, extension, bufferBody, contentType,
+}) => new Promise((resolve) => {
+  // call S3 to retrieve upload file to specified bucket
+  const uploadParams = {
+    Bucket          : config.awsS3BucketName,
+    ACL             : 'public-read', // para poder ser leido publicamente,
+    Key             : `${fileName}.${extension}`,
+    Body            : bufferBody,
+    StorageClass    : 'REDUCED_REDUNDANCY', // para poder ser leido publicamente
+    ContentType     : contentType,
+    ContentEncoding : 'base64',
+  };
 
-    // call S3 to retrieve upload file to specified bucket
-    const uploadParams = {
-      Bucket          : config.awsS3BucketName,
-      ACL             : 'public-read',                // para poder ser leido publicamente,
-      Key             : `${fileName}.${extension}`,
-      Body            : bufferBody,
-      ACL             : 'public-read',                // para poder ser leido publicamente
-      StorageClass    : 'REDUCED_REDUNDANCY',         // para poder ser leido publicamente
-      ContentType     : contentType,
-      ContentEncoding : 'base64',
-    };
+  // Set the region
+  AWS.config.update({ region: config.awsS3Region });
 
-    // Set the region
-    AWS.config.update({region: config.awsS3Region });
-
-    const s3 = new AWS.S3() ;
-    // call S3 to retrieve upload file to specified bucket
-    s3.upload(uploadParams, function (err, data) {
-      if (err) {
-        console.log("Error", err);
-        return resolve({ error : err })
-      } if (data) {
-        console.log("Upload Success", data);
-        // console.log("Upload Success", data.Location);
-        return resolve({ ...data })
-        /***
-        Resultado para generar el QR
-        {
-          ETag: '"51b3ca5a617effc353e9a8a84ef95d1c"',
-          ServerSideEncryption: 'AES256',
-          Location: 'https://myqr.s3.us-east-2.amazonaws.com/eeewqewe.png',
-          key: 'eeewqewe.png',
-          Key: 'eeewqewe.png',
-          Bucket: 'myqr'
-        }
-         */
+  const s3 = new AWS.S3();
+  // call S3 to retrieve upload file to specified bucket
+  // eslint-disable-next-line consistent-return
+  s3.upload(uploadParams, (err, data) => {
+    if (err) {
+      return resolve({ error: err });
+    }
+    if (data) {
+      return resolve({ ...data });
+      /** *
+      Resultado para generar el QR
+      {
+        ETag: '"51b3ca5a617effc353e9a8a84ef95d1c"',
+        ServerSideEncryption: 'AES256',
+        Location: 'https://myqr.s3.us-east-2.amazonaws.com/eeewqewe.png',
+        key: 'eeewqewe.png',
+        Key: 'eeewqewe.png',
+        Bucket: 'myqr'
       }
-    });
-  })
-}
+        */
+    }
+  });
+})
