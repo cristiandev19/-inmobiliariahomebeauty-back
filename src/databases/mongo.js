913419d4046449a02ca4ab0706231b33a11e644b
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-17 23:35:32 -0500

    ajustando espacios

--- src/databases/mongo.js
+++ src/databases/mongo.js
@@ -3,10 +3,10 @@ const { config } = require('../config/index');
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect( config.mongoConnect, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
+    await mongoose.connect(config.mongoConnect, {
+      useNewUrlParser    : true,
+      useUnifiedTopology : true,
+      useCreateIndex     : true,
     });
   } catch (error) {
     throw new Error('Algo salio mal en la coneccion a mongo')
