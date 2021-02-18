5cff8012ad7c2d38fd8c2be17a1254180fb9a06a
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-17 23:35:16 -0500

    variables

--- src/config/index.js
+++ src/config/index.js
@@ -8,7 +8,7 @@ const config = {
   awsS3BucketName   : process.env.AWS_S3_BUCKET_NAME,
   awsS3Region       : process.env.AWS_S3_REGION,
   authJwtSecret     : process.env.AUTH_JWT_SECRET,
-  authJwtExpireTime : process.env.AUTH_JWT_EXPIRE_TIME
+  authJwtExpireTime : process.env.AUTH_JWT_EXPIRE_TIME,
 };
 
 module.exports = { config };
