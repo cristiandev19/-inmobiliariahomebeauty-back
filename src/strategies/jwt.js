9b7f92fc5875791341544f37833a8c354127e3ce
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-17 23:36:24 -0500

    lel

--- src/strategies/jwt.js
+++ src/strategies/jwt.js
@@ -1,25 +1,22 @@
 const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
+const { ExtractJwt } = require('passport-jwt');
 const { config } = require('../config/index');
 const User = require('../models/User');
 
 const opts = {
   jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey : config.jwtSecretKey //normally store this in process.env.secret
+  secretOrKey    : config.jwtSecretKey, // normally store this in process.env.secret
 };
 
 // Aqui debemos cambiarlo para que llame a la BD por un id dentro del token
-module.exports = new JwtStrategy(opts, async (jwt_payload, done) => {
-  console.log('jwt_payload', jwt_payload);
-  User.findOne({_id: jwt_payload.sub}, (err, user) => {
+module.exports = new JwtStrategy(opts, async (jwtPayload, done) => {
+  User.findOne({ _id: jwtPayload.sub }, (err, user) => {
     if (err) {
       return done(err);
     }
     if (!user) {
-      return done(null, false)
+      return done(null, false);
     }
-
-    return done(null, user)
+    return done(null, user);
   });
-
-})
+});
--- src/utilities/images.js
+++ src/utilities/images.js
@@ -1,21 +1,15 @@
-exports.convertBase64ToBuffer = (base64) => {
-  return new Promise((resolve) => {
-    try {
-      /// Proceso para pasar un base64 a un buffer
-      const base64Replaced = base64.replace(/^data:image\/\w+;base64,/, "");
-      const buffer = Buffer.from(base64Replaced, 'base64');
-      const type = base64.split(';')[0].split('/')[1];
-      console.log('type', type)
-      console.log('buffer', buffer);
-      console.log(`ContentType:`, `image/${type}`);
-      return resolve({
-        buffer,
-        type,
-        contentType: `image/${type}`
-      })
-    } catch (error) {
-      return resolve({error})
-    }
-  })
-
-}
+exports.convertBase64ToBuffer = (base64) => new Promise((resolve) => {
+  try {
+    /// Proceso para pasar un base64 a un buffer
+    const base64Replaced = base64.replace(/^data:image\/\w+;base64,/, '');
+    const buffer = Buffer.from(base64Replaced, 'base64');
+    const type = base64.split(';')[0].split('/')[1];
+    return resolve({
+      buffer,
+      type,
+      contentType: `image/${type}`,
+    });
+  } catch (error) {
+    return resolve({ error });
+  }
+});
