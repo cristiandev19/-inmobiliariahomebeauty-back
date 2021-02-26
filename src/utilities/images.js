0718a02a871f977b45dde18bff0af372e5797ca5
cristiandev <cristiansotomayor.dev@gmail.com>
2021-02-25 23:06:03 -0500

    cambio de hoy

--- src/utilities/images.js
+++ src/utilities/images.js
@@ -1,4 +1,7 @@
-exports.convertBase64ToBuffer = (base64) => new Promise((resolve) => {
+const crypto = require('crypto');
+const awsUtilities = require('./aws');
+
+const convertBase64ToBuffer = (base64) => new Promise((resolve) => {
   try {
     /// Proceso para pasar un base64 a un buffer
     const base64Replaced = base64.replace(/^data:image\/\w+;base64,/, '');
@@ -13,3 +16,25 @@ exports.convertBase64ToBuffer = (base64) => new Promise((resolve) => {
     return resolve({ error });
   }
 });
+exports.convertBase64ToBuffer = convertBase64ToBuffer;
+
+// eslint-disable-next-line no-async-promise-executor
+exports.uploadBase64ToS3 = (imgBase64, fileName = '') => new Promise(async (resolve) => {
+  const bufferObjectImg = await convertBase64ToBuffer(imgBase64);
+  // 'imgBase64', 'imgUrl', 'isBase64'
+  const randomString = crypto.randomBytes(20).toString('hex');
+  const fileNameFormated = fileName ? `${fileName}_randomString` : randomString;
+  const uploadParams = {
+    bufferBody  : bufferObjectImg.buffer,
+    contentType : bufferObjectImg.contentType,
+    extension   : bufferObjectImg.type,
+    fileName    : fileNameFormated,
+  };
+  const responseS3 = await awsUtilities.s3UploadPromise(uploadParams);
+  return resolve({
+    url      : responseS3.Location,
+    fileName : responseS3.key,
+    bucket   : responseS3.Bucket,
+  });
+});
+
