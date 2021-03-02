bf6e71d045886cd1743d9a6cba7349dd443194b4
cristiandev <cristiansotomayor.dev@gmail.com>
2021-03-01 23:40:51 -0500

    ya funciona

--- src/modules/admin/admin.controller.js
+++ src/modules/admin/admin.controller.js
@@ -43,10 +43,6 @@ exports.uploadFile = async (req, res, next) => {
 
 exports.createInmueble = async (req, res, next) => {
   try {
-    console.log('req', req.user);
-    return res.status(200).send({
-      message: 'Funciono estas autentificado',
-    });
     const multimediaPromises = await Promise.all(
       [...req.body.multimedia]
         .map(async (multi) => imagesUtilities.uploadBase64ToS3(multi.imgBase64)),
@@ -61,6 +57,7 @@ exports.createInmueble = async (req, res, next) => {
     } = await inmuebleService.createInmueble({
       datosPrincipales : req.body.datosPrincipales,
       caracteristicas  : req.body.caracteristicas,
+      registerUser     : req.user._id,
       multimedia,
     });
 
@@ -71,6 +68,43 @@ exports.createInmueble = async (req, res, next) => {
     return res.status(200).send({
       success,
       inmueble,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+exports.getInmuebles = async (req, res, next) => {
+  try {
+    const {
+      success, inmuebles, error, message,
+    } = await inmuebleService.readInmuebles();
+    if (error) {
+      throw new Error(message);
+    }
+
+    return res.status(200).send({
+      success,
+      inmuebles,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+exports.getInmueble = async (req, res, next) => {
+  try {
+    const { _id } = req.query;
+    const {
+      success, inmuebles, error, message,
+    } = await inmuebleService.readInmueble(_id);
+    if (error) {
+      throw new Error(message);
+    }
+
+    return res.status(200).send({
+      success,
+      inmueble: inmuebles[0],
     });
   } catch (error) {
     return next(error);
