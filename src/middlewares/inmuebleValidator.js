6958562917ff0711f122c33b513e1c2c0ad58b2e
cristiandev <cristiansotomayor.dev@gmail.com>
2021-03-03 00:02:40 -0500

    nuevo validador

--- src/middlewares/inmuebleValidator.js
+++ src/middlewares/inmuebleValidator.js
@@ -28,3 +28,35 @@ exports.createInmuebleValidator = (req, res, next) => {
 
   next();
 };
+
+exports.updateInmuebleValidator = (req, res, next) => {
+  const {
+    datosPrincipales,
+    caracteristicas,
+    inmuebleID,
+  } = req.body;
+  if (!inmuebleID) {
+    return res.status(400).send({
+      message: 'El id del inmueble es obligatorio',
+    });
+  }
+
+  const datosPrincipalesInstance = new DatosPrincipales(datosPrincipales);
+  const caracteristicasInstance = new Caracteristicas(caracteristicas);
+
+  const datosPrincipalesErrors = datosPrincipalesInstance.validateSync();
+  if (datosPrincipalesErrors) {
+    return res.status(400).send({
+      message: 'Campo faltante en datos principales',
+    });
+  }
+  const caracteristicasErrors = caracteristicasInstance.validateSync();
+
+  if (caracteristicasErrors) {
+    return res.status(400).send({
+      message: 'Campo faltante en caracteristicas',
+    });
+  }
+
+  next();
+};
