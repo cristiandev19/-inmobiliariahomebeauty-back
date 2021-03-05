9365de6af68174757ce27d5f0c72d199bb704444
cristiandev <cristiansotomayor.dev@gmail.com>
2021-03-04 23:42:18 -0500

    para que funcione con folder

--- src/utilities/images.js
+++ src/utilities/images.js
@@ -19,16 +19,17 @@ const convertBase64ToBuffer = (base64) => new Promise((resolve) => {
 exports.convertBase64ToBuffer = convertBase64ToBuffer;
 
 // eslint-disable-next-line no-async-promise-executor
-exports.uploadBase64ToS3 = (imgBase64, fileName = '') => new Promise(async (resolve) => {
+exports.uploadBase64ToS3 = (imgBase64, fileName = '', folderPath = '') => new Promise(async (resolve) => {
   const bufferObjectImg = await convertBase64ToBuffer(imgBase64);
   // 'imgBase64', 'imgUrl', 'isBase64'
   const randomString = crypto.randomBytes(20).toString('hex');
-  const fileNameFormated = fileName ? `${fileName}_randomString` : randomString;
+  const fileNameFormated = fileName ? `${fileName}_${randomString}` : randomString;
   const uploadParams = {
     bufferBody  : bufferObjectImg.buffer,
     contentType : bufferObjectImg.contentType,
     extension   : bufferObjectImg.type,
     fileName    : fileNameFormated,
+    folderPath  : folderPath ? `${folderPath}/` : '',
   };
   const responseS3 = await awsUtilities.s3UploadPromise(uploadParams);
   return resolve({
@@ -38,4 +39,3 @@ exports.uploadBase64ToS3 = (imgBase64, fileName = '') => new Promise(async (reso
     extension : bufferObjectImg.type,
   });
 });
-
